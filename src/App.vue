1063706c5a96e538ae5b972cba0734316199fe64
limjihoon99 <bo9701@naver.com>
2024-08-09 00:53:54 +0900

    feat: 허위매물신고 기능 추가

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,23 @@
     <div class="menu">
       <a v-for="a in menus" :key="a">{{a}}</a>
     </div>
-    <div v-for="a in products" :key="a">
-      <h4>{{ a }}</h4>
+    <div>
+      <img src="./assets/room0.jpg" class="room-img">
+      <h4>{{ products[0] }}</h4>
       <p>50 만원</p>
+      <button @click="report[0] += 1">허위 매물 신고</button> <span>신고 수: {{report[0]}}</span>
+    </div>
+    <div>
+      <img src="./assets/room1.jpg" class="room-img">
+      <h4>{{ products[1] }}</h4>
+      <p>60 만원</p>
+      <button @click="report[1] += 1">허위 매물 신고</button> <span>신고 수: {{report[1]}}</span>
+    </div>
+    <div>
+      <img src="./assets/room2.jpg" class="room-img">
+      <h4>{{ products[2] }}</h4>
+      <p>70 만원</p>
+      <button @click="report[2] += 1">허위 매물 신고</button> <span>신고 수: {{report[2]}}</span>
     </div>
   </div>
 </template>
@@ -17,8 +31,14 @@ export default {
   name: 'App',
   data() {
     return {
+      report: [0, 0, 0],
       menus: ['Home', 'Shop', 'About'],
       products: ['역삼동원룸', '천호동원룸', '마포구원룸'],
+    }
+  },
+  methods: {
+    increase() {
+      this.report += 1;
     }
   },
   components: {
@@ -27,6 +47,11 @@ export default {
 </script>
 
 <style>
+.room-img {
+  width: 100%;
+  margin-top: 40px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
