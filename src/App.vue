f074e4ca92680e349d46e49869138079669ee65d
limjihoon99 <bo9701@naver.com>
2024-08-09 00:34:14 +0900

    feat: Create Vue file

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+
+    <div class="menu">
+      <a v-for="a in menus" :key="a">{{a}}</a>
+    </div>
+    <div v-for="a in products" :key="a">
+      <h4>{{ a }}</h4>
+      <p>50 만원</p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      menus: ['Home', 'Shop', 'About'],
+      products: ['역삼동원룸', '천호동원룸', '마포구원룸'],
+    }
+  },
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
+}
+</style>
